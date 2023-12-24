43217b25bbc46afc8cf8632f9658e22a670359e8
sokey <126024643+SOK3Y@users.noreply.github.com>
2023-12-24 02:42:12 -0800

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,25 @@
+Config = {}
+
+Config.Blips = {
+    {
+		name = 'Warsztat Paleto',
+		sprite = 96,
+		scale = 0.8,
+		color = 47,
+		coords = vector3(113.35631561279,6618.9780273438,31.854057312012),
+	},
+    {
+		name = "Benny's",
+		sprite = 96,
+		scale = 0.8,
+		color = 6,
+		coords = vector3(-211.54264831543, -1324.8336181641, 30.890382766724),
+	},
+    -- {
+	-- 	name = "Benny's",
+	-- 	sprite = 96,
+	-- 	scale = 0.8,
+	-- 	color = 6,
+	-- 	coords = vector3(-211.54264831543, -1324.8336181641, 30.890382766724),
+	-- },
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "cerulean"
+game "gta5"
+lua54 'yes'
+
+name "Blips"
+author "sokey"
+
+
+client_scripts {
+     "client.lua",
+     "config.lua"
+}
+
